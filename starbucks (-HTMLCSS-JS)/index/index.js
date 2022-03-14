4782f4f36ebe082b998a5613564af4e0b1bea25b
eslleydev <92341553+eslleydev@users.noreply.github.com>
2022-03-14 14:58:30 -0300

    Add files via upload

---
+++ starbucks (-HTMLCSS-JS)/index/index.js
@@ -0,0 +1,8 @@
+function imgSlider (anything){
+    document.querySelector('.starbucks').src = anything
+}
+
+function changneCircleColor (color) {
+    const circle = document.querySelector ('.circle')
+    circle.style.background = color
+}
